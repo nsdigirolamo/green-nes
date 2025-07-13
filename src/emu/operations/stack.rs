cb89faeef7b51fe4660320f38d0f31c0a95a88ed
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-07-13 15:04:59 -0400

    fixed plp again

--- src/emu/operations/stack.rs
+++ src/emu/operations/stack.rs
@@ -14,7 +14,8 @@ pub fn php(state: &mut State) {
 pub fn plp(state: &mut State) {
     // B and extra bit are ignored
     let masked_stack_status = state.read_from_memory(state.address_bus) & 0b_1100_1111;
-    let new_processor_status = masked_stack_status | state.processor_status_register;
+    let masked_processor_status = state.processor_status_register & 0b_0011_0000;
+    let new_processor_status = masked_stack_status | masked_processor_status;
     // @TODO: The effect of changing the I flag should be delayed by one instruction.
     state.processor_status_register = new_processor_status
 }
