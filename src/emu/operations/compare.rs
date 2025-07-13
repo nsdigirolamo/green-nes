560ec63dc2d3725b9a9e2453e6e2ddaf0cfbfd1d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-07-13 14:10:53 -0400

    Fixed flag errors with comparison ops

--- src/emu/operations/compare.rs
+++ src/emu/operations/compare.rs
@@ -2,11 +2,11 @@ use crate::emu::{half_cycles::get_effective_address, state::State};
 
 pub fn cmp(state: &mut State) {
     let data = state.read_from_memory(state.address_bus);
-    let (result, overflow) = state.accumulator.overflowing_sub(data);
+    let result = state.accumulator.wrapping_sub(data);
 
-    state.set_carry_flag(overflow);
-    state.set_zero_flag(result == 0);
-    state.set_negative_flag((result >> 7) == 1);
+    state.set_carry_flag(state.accumulator >= data);
+    state.set_zero_flag(state.accumulator == data);
+    state.set_negative_flag((result & 0b_1000_0000) != 0);
 }
 
 pub fn cmp_indirect_y(state: &mut State) {
@@ -27,18 +27,18 @@ pub fn cmp_absolute_indexed(state: &mut State) {
 
 pub fn cpx(state: &mut State) {
     let data = state.read_from_memory(state.address_bus);
-    let (result, overflow) = state.x_index_register.overflowing_sub(data);
+    let result = state.x_index_register.wrapping_sub(data);
 
-    state.set_carry_flag(overflow);
-    state.set_zero_flag(result == 0);
-    state.set_negative_flag((result >> 7) == 1);
+    state.set_carry_flag(state.x_index_register >= data);
+    state.set_zero_flag(state.x_index_register == data);
+    state.set_negative_flag((result & 0b_1000_0000) != 0);
 }
 
 pub fn cpy(state: &mut State) {
     let data = state.read_from_memory(state.address_bus);
-    let (result, overflow) = state.y_index_register.overflowing_sub(data);
+    let result = state.y_index_register.wrapping_sub(data);
 
-    state.set_carry_flag(overflow);
-    state.set_zero_flag(result == 0);
-    state.set_negative_flag((result >> 7) == 1);
+    state.set_carry_flag(state.y_index_register >= data);
+    state.set_zero_flag(state.y_index_register == data);
+    state.set_negative_flag((result & 0b_1000_0000) != 0);
 }
