8b4d2a1434fa098709d254bb7973aadf461a8332
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-07-23 20:24:45 -0400

    added absolute x nops

--- src/emu/operations/other.rs
+++ src/emu/operations/other.rs
@@ -1,3 +1,9 @@
-use crate::emu::state::State;
+use crate::emu::{half_cycles::get_effective_address, state::State};
 
 pub fn nop(_: &mut State) {}
+
+pub fn nop_absolute_indexed(state: &mut State) {
+    if state.crossed_page {
+        state.cycle_queue.push_back([get_effective_address, nop]);
+    }
+}
