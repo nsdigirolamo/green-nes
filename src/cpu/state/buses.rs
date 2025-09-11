3fc48b83acb135a0bd3e1557655981f81a34e2cf
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-09-10 22:17:50 -0400

    implemented proper memory mapping

--- src/cpu/state/buses.rs
+++ src/cpu/state/buses.rs
@@ -1,5 +1,22 @@
 use crate::concat_u8;
 
+pub const RAM_MIN_ADDRESS: u16 = 0x0000;
+pub const RAM_MAX_ADDRESSS: u16 = 0x07FF;
+pub const RAM_MAX_MIRRORED_ADDRESS: u16 = 0x1FFF;
+
+pub const PPU_REGISTER_MIN_ADDRESS: u16 = 0x2000;
+pub const PPU_REGISTER_MAX_ADDRESS: u16 = 0x2007;
+pub const PPU_REGISTER_MAX_MIRRORED_ADDRESS: u16 = 0x3FFF;
+
+pub const APU_IO_MIN_ADDRESS: u16 = 0x4000;
+pub const APU_IO_MAX_ADDRESS: u16 = 0x4017;
+
+pub const TEST_APU_IO_MIN_ADDRESS: u16 = 0x4018;
+pub const TEST_APU_IO_MAX_ADDRESS: u16 = 0x401F;
+
+pub const UNMAPPED_MIN_ADDRESS: u16 = 0x4020;
+pub const UNMAPPED_MAX_ADDRESS: u16 = 0xFFFF;
+
 pub const MEMORY_SIZE: usize = 65536;
 
 pub struct Buses {
@@ -28,14 +45,52 @@ impl Default for Buses {
 
 impl Buses {
     pub fn read(&mut self, address: (u8, u8)) -> u8 {
-        let data = self.memory[concat_u8!(address.0, address.1) as usize];
-        self.data = data;
+        let address = concat_u8!(address.0, address.1);
 
-        data
+        match address {
+            RAM_MIN_ADDRESS..=RAM_MAX_MIRRORED_ADDRESS => {
+                let mirror_address = address & 0b_0000_0111_1111_1111;
+                let data = self.memory[mirror_address as usize];
+                self.data = data;
+
+                data
+            }
+            PPU_REGISTER_MIN_ADDRESS..=PPU_REGISTER_MAX_MIRRORED_ADDRESS => {
+                todo!("ppu not yet supported")
+            }
+            APU_IO_MIN_ADDRESS..=APU_IO_MAX_ADDRESS => {
+                todo!("apu not yet supported")
+            }
+            TEST_APU_IO_MIN_ADDRESS..=TEST_APU_IO_MAX_ADDRESS => {
+                todo!("apu test not yet supported")
+            }
+            UNMAPPED_MIN_ADDRESS..=UNMAPPED_MAX_ADDRESS => {
+                todo!("unmapped region")
+            }
+        }
     }
 
     pub fn write(&mut self, address: (u8, u8), data: u8) {
-        self.data = data;
-        self.memory[concat_u8!(address.0, address.1) as usize] = data;
+        let address = concat_u8!(address.0, address.1);
+
+        match address {
+            RAM_MIN_ADDRESS..=RAM_MAX_MIRRORED_ADDRESS => {
+                let mirror_address = address & 0b_0000_0111_1111_1111;
+                self.data = data;
+                self.memory[mirror_address as usize] = data;
+            }
+            PPU_REGISTER_MIN_ADDRESS..=PPU_REGISTER_MAX_MIRRORED_ADDRESS => {
+                todo!("ppu not yet supported")
+            }
+            APU_IO_MIN_ADDRESS..=APU_IO_MAX_ADDRESS => {
+                todo!("apu not yet supported")
+            }
+            TEST_APU_IO_MIN_ADDRESS..=TEST_APU_IO_MAX_ADDRESS => {
+                todo!("apu test not yet supported")
+            }
+            UNMAPPED_MIN_ADDRESS..=UNMAPPED_MAX_ADDRESS => {
+                todo!("unmapped region")
+            }
+        }
     }
 }
