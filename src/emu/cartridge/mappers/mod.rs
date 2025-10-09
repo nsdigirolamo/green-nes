e801238253bc87ea80978a6086a8ac8f562e6950
Nicholas DiGirolamo <nsdigirolamo@pm.me>
2025-10-09 19:53:07 -0400

    Added cart mapper 0

---
+++ src/emu/cartridge/mappers/mod.rs
@@ -0,0 +1,8 @@
+pub mod nrom;
+
+pub trait Mapper {
+    fn prg_read(&self, addr: u16) -> u8;
+    fn prg_write(&self, addr: u16, data: u8);
+    fn chr_read(&self, addr: u16) -> u8;
+    fn chr_write(&self, addr: u16, data: u8);
+}
